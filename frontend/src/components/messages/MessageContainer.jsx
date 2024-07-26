5bc67e2e2dd3d7ecb47e8486e9667553bef842e0
DissDude <rohit.suthar.1935@gmail.com>
2024-07-25 21:24:34 -0700

    Message container updated

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -5,47 +5,40 @@ import useConversation from '../../zustand/useConversation'
 import { AuthContext, useAuthContext } from '../../context/AuthContext';
 
 function MessageContainer() {
+  const { selectedConversation, setSelectedConversation } = useConversation();
 
-      const {selectedConversation, setSelectedConversation}=useConversation();
-
-      //cleanup function
-      useEffect(()=>{
-          return () =>  setSelectedConversation(null);
-      }, [setSelectedConversation])
+  // Cleanup function
+  useEffect(() => {
+    return () => setSelectedConversation(null);
+  }, [setSelectedConversation]);
 
   return (
-  
-        <div className='min-w-24 flex flex-col  '>
-          
-           {!selectedConversation ? (<NoChatSelected/>) : (
-         <>
-             
-               <div className=' bg-gray-700 px-40 py-4 mb-2 '>
-                <span className='label-text '>To:</span><span className='text-gray-900 font-bold'>{selectedConversation.fullName}</span>
-               </div>
-               <Messages/>
-               <MessageInput/>
-         </>
-)}
-        </div>
-
-  )
+    <div className='min-w-full flex flex-col sm:min-w-24'>
+      {!selectedConversation ? (<NoChatSelected />) : (
+        <>
+          <div className='bg-gray-700 px-4 sm:px-10 py-4 mb-2 flex items-center'>
+            <span className='label-text text-gray-200'>To: </span>
+            <span className='text-gray-100 font-bold ml-2'>{selectedConversation.fullName}</span>
+          </div>
+          <Messages />
+          <MessageInput />
+        </>
+      )}
+    </div>
+  );
 }
 
-export default MessageContainer
+export default MessageContainer;
 
-const NoChatSelected = () =>{
+const NoChatSelected = () => {
+  const { authUser } = useAuthContext();
 
-      const{authUser} = useAuthContext();
-
-      return(
-               <div className='flex items-center justify-center w-full h-full'>
-
-                  <div className='px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2'>
-                        <p>Welcome ${authUser.username}</p>
-                        <p>Select a chat to start messaging</p>
-
-                  </div>
-               </div>
-      )
+  return (
+    <div className='flex items-center justify-center w-full h-full'>
+      <div className='px-4 text-center text-sm sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2'>
+        <p>Welcome {authUser.username}</p>
+        <p>Select a chat to start messaging</p>
+      </div>
+    </div>
+  );
 }
