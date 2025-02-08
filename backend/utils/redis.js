f5cb0540a840754372ab1651124210cdc5e946f0
rohit.suthar.1935 <rohit.suthar.1935@gmail.com>
2025-02-08 20:45:43 +0000

    Bug FIx

--- backend/utils/redis.js
+++ backend/utils/redis.js
@@ -6,7 +6,7 @@ dotenv.config();
 export const redis = new Redis({
     host: 'redis-11449.crce179.ap-south-1-1.ec2.redns.redis-cloud.com',
     port: 11449,
-    username: 'student',
+    username: 'default',
     password: process.env.REDIS_PASSWORD,  // Use your actual password
   });
 
