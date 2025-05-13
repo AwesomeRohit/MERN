c25a3a45fb02ab79b34acd850450a6c2345b219f
Rohit Suthar <44650354+AwesomeRohit@users.noreply.github.com>
2025-05-13 14:29:26 +0530

    Redis Database Updated

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,34 +1,19 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
-import {
-    getRecieverSocketId,
-    io
-} from "../socket/socket.js";
+import {getRecieverSocketId,io} from "../socket/socket.js";
 import client from "ioredis";
-import {
-    redis
-} from "../utils/redis.js";
+import {redis} from "../utils/redis.js";
 
 export const sendMessage = async (req, res) => {
-
     try {
-        const {message} = req.body;
-        const {id: recieverID} = req.params;
+        const { message } = req.body;
+        const { id: recieverID } = req.params;
         const senderID = req.user._id;
 
-        let conversation = await Conversation.findOne({participants: {$all: [senderID, recieverID]}})
-        
-        if (!conversation) { conversation = await Conversation.create({
-                participants: [senderID, recieverID]
-            })
-
-        }
-
-        const newMessage = new Message({senderID,recieverID,message})
-
-        if (newMessage) {
-            conversation.messages.push(newMessage._id);
-        }
+        let conversation = await Conversation.findOne({ participants: { $all: [senderID, recieverID] } })
+        if (!conversation) { conversation = await Conversation.create({participants: [senderID, recieverID]});}
+        const newMessage = new Message({ senderID, recieverID, message })
+        if (newMessage) {conversation.messages.push(newMessage._id);}
 
 
         // we optimized the code by using promises and running both of the save functions at the same time for less time consuming
@@ -56,52 +41,35 @@ export const sendMessage = async (req, res) => {
 
     } catch (error) {
         console.log("Error in sendMessage controller", error.message);
-        res.status(500).json({error: "Internal Server Error"});
+        res.status(500).json({ error: "Internal Server Error" });
     }
 
     console.log("sent", req.params.id);
 }
 export const getMessage = async (req, res) => {
-
     try {
-        const {
-            id: userToChatId
-        } = req.params;
-
+        const {id: userToChatId} = req.params;
         const senderID = req.user._id;
-
-
         //generate cachekey for redis
         const cacheKey = `chat:messages:${senderID}:${userToChatId}`;
-
         const cachedMessages = await redis.get(cacheKey);
         if (cachedMessages) return res.status(200).json(JSON.parse(cachedMessages));
-
-        const conversation = await Conversation.findOne({
-
-            participants: {
-                $all: [senderID, userToChatId]
-            },
-
-        }).populate("messages");
-
+        const conversation = await Conversation.findOne({participants: {$all: [senderID, userToChatId]},}).populate("messages");
         if (!conversation) return res.status(200).json([]);
-
         const messages = conversation.messages;
-
-        await redis.set(cacheKey, JSON.stringify(messages),'EX', 3600); // Cache for 1 hour
-
+        await redis.set(cacheKey, JSON.stringify(messages), 'EX', 3600); // Cache for 1 hour
         res.status(200).json(messages);
-
-
-
     } catch (error) {
-
         console.log("Error in getMessage controller", error.message);
-
-        res.status(500).json({
-            error: "Internal Server Error"
-        });
+        res.status(500).json({error: "Internal Server Error"});
 
     }
 }
+
+
+ const fetch = new Promise((resolve, reject)=>{
+    setInterval(()=>{
+        const data = fetch(url)
+    },1000)
+ })
+ .then(error => console.log(error.message))
--- backend/utils/redis.js
+++ backend/utils/redis.js
@@ -4,8 +4,8 @@ import dotenv from "dotenv"
 dotenv.config();
 
 export const redis = new Redis({
-    host: 'redis-11030.crce179.ap-south-1-1.ec2.redns.redis-cloud.com',
-    port: 11030,
+    host: 'redis-12820.c264.ap-south-1-1.ec2.redns.redis-cloud.com',
+    port: 12820,
     username: 'default',
     password: process.env.REDIS_PASSWORD,  // Use your actual password
   });
